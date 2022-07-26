7ff5b041dd0c9745590223be9fbf613dbd1576da
unknown <mohamud2611@outlook.com>
2022-07-26 18:52:45 +0530

    modfiy

---
+++ program1/src/Screen/Booking.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Booking() {
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  )
+}
+
+export default Booking
--- program1/src/Screen/Home.js
+++ program1/src/Screen/Home.js
@@ -1,13 +1,30 @@
 import React from 'react'
+// import Booking from './Booking'
+// import { useNavigate } from 'react-router'
 
 function Home() {
+  // const navigate=useNavigate();
+
+  const Booking = () => 
+    alert({
+      message:'success',type:'succes'
+    });
+   // return(
+    //   <div>
+    //     <input type='text' placeholder='Phoneno' value='none' required='10 number'/>
+    //   </div>
+    // )
+    // alert("welcome")
+    // navigate('/Booking');
+  
   return (
     <div className='homebody'>
       <div className="booking">
         <h2> See the world with your own two eyes</h2>
         <h3> Keep calm and never stop travelling</h3>
-        <button >Book</button>
+        <button id='alert' onClick={Booking}>Book</button>
       </div>
+      
     </div>
   )
 }
